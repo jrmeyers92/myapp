3db10dcf4baaae850c2501ea67c79ccbeb480ca1
Jake Meyers <JakeRMeyers92@gmail.com>
2020-09-08 16:39:17 -0600

    added jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const buttonText = {text: 'click me'};
+  const labelText = "Enter Name:"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label className="label" for="name">{labelText}</label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor: 'blue', color: 'white'}}>
+        {buttonText.text}
+      </button>
+
     </div>
   );
 }
